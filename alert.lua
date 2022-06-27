f02821a3131c87e5ebfaa3e28c5aec4a1e90dd95
RecalsZ <94567250+RecalsZ@users.noreply.github.com>
2022-06-27 22:19:19 +0100

    Update alert.lua

--- alert.lua
+++ alert.lua
@@ -1,7 +1,7 @@
 -- Copyright (c) 2022 Lion Kortlepel. Use allowed under MIT license.
 
 local minutes = 10
-local message = "This server is hosted for free by ConnectHosting.net"
+local message = "This server is hosted for free by ConnectHosting. Join the discord discord.gg/Dz3zTMSeuy"
 
 function discordReminder()
 	MP.SendChatMessage(-1, message)
